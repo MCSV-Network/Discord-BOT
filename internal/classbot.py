c48c97945f1423bda0105ed1d9456530abdf9a8c
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-02-23 23:38:59 +0900

    彼はimportの存在を忘れていた

--- internal/classbot.py
+++ internal/classbot.py
@@ -1,3 +1,11 @@
+import asyncio
+import datetime
+import time
+from pathlib import Path
+
+import discord
+from nextcord.ext import commands
+
 class ringoBot(commands.Bot):
 	def __init__(self, command_prefix):
 		super().__init__(command_prefix)
