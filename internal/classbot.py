33f56e6786c4c355273e3e561a212a50b33c6122
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-02-28 23:23:55 +0900

    きっとこいつのせいだ

--- internal/classbot.py
+++ internal/classbot.py
@@ -13,16 +13,10 @@ DiscordBot_Cogs = [
 ]
 
 class ringoBot(commands.Bot):
-	async def __init__(self, command_prefix):
+	def __init__(self, command_prefix):
 		super().__init__(command_prefix)
-		await self.wait_until_ready()
-		await asyncio.sleep(1)  # Ensure that on_ready has completed and finished printing
-		cogs = [x.stem for x in Path('cogs').glob('*.py')]
-		for extension in cogs:
+		for cogs in DiscordBot_Cogs:
 			try:
-				print(f'loaded {extension}')
-				self.load_extension(f'cogs.{extension}')
-			except Exception as e:
-				error = f'{extension}\n {type(e).__name__} : {e}'
-				print(f'failed to load extension {error}')
-			print('-' * 10)
+				self.load_extension(cogs)
+			except Exception:
+				traceback.print_exec()
