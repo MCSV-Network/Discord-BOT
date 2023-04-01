d577512222e485000cfaa985f9425fa3c244e7f9
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-04-02 02:39:56 +0900

    update checkconfirm.py

--- internal/checkconfirm.py
+++ internal/checkconfirm.py
@@ -12,18 +12,21 @@ async def confirm(ctx: commands.Context, message: discord.Message):
     
     try:
         dcmoji.say() # この行が必要な場合は、tryブロックに移動する必要があります。
+
+        confirm_emoji = dcmoji.CONFIRM_REACTION_EMOJI
+        cancel_emoji = dcmoji.CANCEL_REACTION_EMOJI
         
         def check(r, u):
-            return str(r.dcmoji) in (dcmoji.CONFIRM_REACTION_EMOJI, dcmoji.CANCEL_REACTION_EMOJI) and u.id == ctx.author.id and r.message.id == message.id
+            return str(r.dcmoji) in (confirm_emoji, cancel_emoji) and u.id == ctx.author.id and r.message.id == message.id
 
-        await message.add_reaction(dcmoji.CONFIRM_REACTION_EMOJI)
-        await message.add_reaction(dcmoji.CANCEL_REACTION_EMOJI)
+        await message.add_reaction(confirm_emoji)
+        await message.add_reaction(cancel_emoji)
 
         reaction, user = await ctx.bot.wait_for('reaction_add', timeout=10.0, check=check)
 
-        dcmoji = str(reaction.dcmoji)
+        selected_emoji = str(reaction.dcmoji)
 
-        if dcmoji == dcmoji.CONFIRM_REACTION_EMOJI:
+        if selected_emoji == confirm_emoji:
             return True
         return False
     except asyncio.TimeoutError:
