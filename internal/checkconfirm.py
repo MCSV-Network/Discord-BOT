177b554b226f0bfc1bcae09efc24fb1f428b4279
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-04-02 02:24:51 +0900

    こんなので直るのかな

--- internal/checkconfirm.py
+++ internal/checkconfirm.py
@@ -4,25 +4,24 @@ import asyncio
 from internal import dcmoji
 
 async def confirm(ctx: commands.Context, message: discord.Message):
-    dcmoji.say()
     """
     Creates a confirm/cancel reaction menu that returns True or False depending on which reaction was clicked.
 
     If a timeout occurs, it will return None.
     """
-
-    dcmoji = ""
-
-    def check(r, u):
-        nonlocal dcmoji
-        dcmoji = str(r.dcmoji)
-        return dcmoji in (dcmoji.CONFIRM_REACTION_EMOJI, dcmoji.CANCEL_REACTION_EMOJI) and u.id == ctx.author.id and r.message.id == message.id
-
-    await message.add_reaction(dcmoji.CONFIRM_REACTION_EMOJI)
-    await message.add_reaction(dcmoji.CANCEL_REACTION_EMOJI)
-
+    
     try:
+        dcmoji.say() # この行が必要な場合は、tryブロックに移動する必要があります。
+        
+        def check(r, u):
+            return str(r.dcmoji) in (dcmoji.CONFIRM_REACTION_EMOJI, dcmoji.CANCEL_REACTION_EMOJI) and u.id == ctx.author.id and r.message.id == message.id
+
+        await message.add_reaction(dcmoji.CONFIRM_REACTION_EMOJI)
+        await message.add_reaction(dcmoji.CANCEL_REACTION_EMOJI)
+
         reaction, user = await ctx.bot.wait_for('reaction_add', timeout=10.0, check=check)
+
+        dcmoji = str(reaction.dcmoji)
 
         if dcmoji == dcmoji.CONFIRM_REACTION_EMOJI:
             return True
