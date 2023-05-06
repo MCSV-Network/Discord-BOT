e67c01a077ead86f167b7f656b2324a4d67b92af
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-05-06 23:38:09 +0900

    ummm

--- internal/ringostatus.py
+++ internal/ringostatus.py
@@ -1,23 +1,24 @@
 import asyncio
 import nextcord as discord
+from nextcord.ext import commands
 from random import randrange as rr
 
-activity = discord.Activity(name="起動中…", type=discord.ActivityType.playing)
-client = discord.Client(activity=activity)
-@client.event
-
-async def iamringostatus():
+def iamringostatus():
 	print('Loaded ringostatus py!')
 
+activity = discord.Activity(name="起動中…", type=discord.ActivityType.playing)
+bot = discord.Client(activity=activity)
+
+@bot.event
 async def startrpc():
    print("Login completed")
    print('------')
    while True:
-       await client.change_presence(activity = discord.Activity(name="実験中のbotだよ!", type=discord.ActivityType.playing))
+       await bot.change_presence(activity = discord.Activity(name="実験中のbotだよ!", type=discord.ActivityType.playing))
        await asyncio.sleep(15)
-       joinserver=len(client.guilds)
+       joinserver=len(bot.guilds)
        servers=str(joinserver)
-       await client.change_presence(activity = discord.Activity(name="サーバー数:"+servers, type=discord.ActivityType.playing))
+       await bot.change_presence(activity = discord.Activity(name="サーバー数:"+servers, type=discord.ActivityType.playing))
        await asyncio.sleep(15)
-       await client.change_presence(activity = discord.Activity(name="乱数:"+str(rr(0,101)), type=discord.ActivityType.playing))
+       await bot.change_presence(activity = discord.Activity(name="乱数:"+str(rr(0,101)), type=discord.ActivityType.playing))
        await asyncio.sleep(15)
